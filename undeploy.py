39ebc65c45e2c0e39de118c188ac798611aa808e
Niels Madan <nielsmadan@googlemail.com>
2012-05-10 10:44:58 +0200

    scripts: added deploy.py and undeploy.py linux scripts.

---
+++ undeploy.py
@@ -0,0 +1,14 @@
+from subprocess import call
+import os
+
+c_dir = os.path.dirname(os.path.abspath(__file__))
+h_dir = os.getenv("HOME")
+
+print "Home directory found: %s" % h_dir
+print "current directory found: %s\n" % c_dir
+
+rc_file = ['.vimrc', '.gvimrc', '.hgrc', '.vim']
+
+print "Remove rc file symlinks."
+for f in rc_file:
+    call(["rm", os.path.join(h_dir, f)])
