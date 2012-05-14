092122b4a74a3f9139864492b8610ae0e29f8670
Niels Madan <nielsmadan@googlemail.com>
2012-05-14 11:35:42 +0200

    deploy: deploy and undeploy pylintrc

--- undeploy.py
+++ undeploy.py
@@ -7,7 +7,7 @@ h_dir = os.getenv("HOME")
 print "Home directory found: %s" % h_dir
 print "current directory found: %s\n" % c_dir
 
-rc_file = ['.vimrc', '.gvimrc', '.hgrc', '.vim', '.bashrc']
+rc_file = ['.vimrc', '.gvimrc', '.hgrc', '.vim', '.bashrc', '.pylintrc']
 
 print "Remove rc file symlinks."
 for f in rc_file:
